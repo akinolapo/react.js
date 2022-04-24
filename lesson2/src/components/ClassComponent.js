3ff2d69e5794ac6afc1b76cba856c6443dc65627
Peace Akinola <akinolapo@gmail.com>
2022-04-24 23:17:26 +0100

    4th commit modified

--- lesson2/src/components/ClassComponent.js
+++ lesson2/src/components/ClassComponent.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import Button from './Button'
 
 export default class ClassComponent extends Component {
   render() {
     return (
       <div>
           <h2>This is a Class Component</h2>
+          <Button props={"class button"}/>
       </div>
     )
   }
--- lesson2/src/components/FunctionalComponent.js
+++ lesson2/src/components/FunctionalComponent.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Button from './Button'
 
 export default function FunctionalComponent() {
   return (
     <div>
         <h1>This is a Functional Component</h1>
+        <Button props={"func button"}/>
     </div>
   )
 }
