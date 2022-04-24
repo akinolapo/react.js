161c94c125599e90303b9e7b205dadf6152b5a09
Peace Akinola <akinolapo@gmail.com>
2022-04-24 22:51:34 +0100

    4th commit

--- lesson2/src/App.js
+++ lesson2/src/App.js
@@ -1,35 +1,41 @@
 // import logo from './logo.svg';
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+//   Link
+// } from "react-router-dom";
+// import Home from './components/Home';
+// import About from './components/About';
+// import Contact from './components/Contact';
+import FunctionalComponent from './components/FunctionalComponent';
+import ClassComponent from './components/ClassComponent';
 
 
 
 
 function App() {
   return (
+    <div className="App">
+      <FunctionalComponent/>
+      <ClassComponent/>
+    </div>
 
-    <Router>
-      <div className="App">
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About Us</Link></li>
-          <li><Link to="/contact">Contact Us</Link></li>
-        </ul>
-      </div>
-      <Routes>
-        <Route exact path="/" element={< Home />}></Route>
-        <Route exact path="/about" element={< About />}></Route>
-        <Route exact path="/contact" element={< Contact />}></Route>
-      </Routes>
-    </Router>
+    // <Router>
+    //   <div className="App">
+    //     <ul>
+    //       <li><Link to="/">Home</Link></li>
+    //       <li><Link to="/about">About Us</Link></li>
+    //       <li><Link to="/contact">Contact Us</Link></li>
+    //     </ul>
+    //   </div>
+    //   <Routes>
+    //     <Route exact path="/" element={< Home />}></Route>
+    //     <Route exact path="/about" element={< About />}></Route>
+    //     <Route exact path="/contact" element={< Contact />}></Route>
+    //   </Routes>
+    // </Router>
   );
 }
 
--- lesson2/src/components/ClassComponent.js
+++ lesson2/src/components/ClassComponent.js
@@ -4,7 +4,7 @@ export default class ClassComponent extends Component {
   render() {
     return (
       <div>
-          <h2>This is a class component</h2>
+          <h2>This is a Class Component</h2>
       </div>
     )
   }
